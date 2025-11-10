df0f11103f67487801b78f5868bfeb9bdbc5e370
Grimhem7 <112320464+Grimhem7@users.noreply.github.com>
2025-11-10 17:36:05 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/admin',
+        destination: '/login', // Redirect /admin to /login
+      },
+    ];
+  },
+  swcMinify: true,
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+  reactStrictMode : false,
+};
